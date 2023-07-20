b06acec1161f4e94fa2209d358467957d82ff2d5
roshantrivedi <roshankumar.rk99@gmail.com>
2023-07-21 02:44:49 +0530

    commit-4

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,47 @@
+import { useState, useEffect } from "react";
+
+import { SWIGGY_API } from "../utils/constants";
+
 import Search from "./Search";
 import ResCard from "./ResCard";
 
 import resList from "../utils/demoData";
 
 const Body = () => {
+
+    const [restList, setResList] = useState([]);
+
+    function handleClick(){
+        const filteredList = restList.filter( (restaurant) => restaurant.rating > 4);
+        setResList(filteredList);
+    }
+
+    function resetFilter(){
+        setResList(resList);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch(SWIGGY_API);
+
+        const json = await data.json();
+        setResList(json?.data?.cards[2]?.data?.data?.cards);
+        console.log(json);
+    }
+
     return(
         <div className='body'>
             <Search/>
+            <button className="filter-btn" onClick={handleClick} >Click to Filter Rating above 4</button>
+            <button className="filter-btn" onClick={resetFilter}>Reset Filter</button>
             <div className='res-container'>
                 {
-                    resList.map(function (item) {
+                    restList.map(function (item) {
                         return (
-                            <ResCard key={item.id} resList={item}/>
+                            <ResCard key={item.data.id} resList={item.data}/>
                         );
                     })
                 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,6 @@
 export const REST_URL = 'https://www.cubesnjuliennes.com/wp-content/uploads/2020/07/Chicken-Biryani-Recipe.jpg';
 
 export const LOGO_URL = 'https://cdn.dribbble.com/userupload/3948351/file/original-19d7d7a1cbb8c7693758b740d5477d54.jpg?resize=400x0';
+
+export const SWIGGY_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING";
+
